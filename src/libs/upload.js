1e3604f4b7ab0a2d57905c3de2b7678f02dfc18a
Ms-sesia <zoenea@naver.com>
2020-12-14 20:21:15 +0900

    upload
    * post-fileUpload
    add upload api.

---
+++ src/libs/upload.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, `${path.join(__dirname, "../../", "Uploads")}`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).array("file");
+
+export const upload = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) return res.status(500).json(err);
+    next();
+  });
+};
+
+export const uploadController = (req, res) => {
+  const { files } = req;
+  const location = [];
+  //resize 필요하면 추가.
+  files.map((file) => location.push(process.env.LOCALSTORAGEADDR + file.filename));
+
+  return res.status(200).json(location);
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
 import schema from "./schema";
+import { uploadPost, uploadPostController } from "./libs/upload";
 
 const server = new GraphQLServer({
   schema,
@@ -9,5 +10,6 @@ const server = new GraphQLServer({
 });
 
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadPost, uploadPostController);
 
 server.start(() => console.log("Server is running on localhost:4000"));
