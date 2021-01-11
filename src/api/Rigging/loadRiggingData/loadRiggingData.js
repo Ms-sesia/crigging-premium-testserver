b9a429b59a48bbfb776a9917cdafdbe6a8f06626
Ms-sesia <zoenea@naver.com>
2021-01-11 20:57:49 +0900

    loadRiggingData, model modified, editUser
    * loadRiggingData
    - made new API
    - more concept need
    
    * editUser
    - avatar added in Mutation input.
    - load avatar from User DB.
    
    * model
    - require modified.
    - avatar added in model

--- src/api/Rigging/loadRiggingData/loadRiggingData.js
+++ src/api/Rigging/loadRiggingData/loadRiggingData.js
@@ -10,19 +10,75 @@ export default {
       const { userId } = args.userId;
       const craneData = await prisma.craneData.findMany();
       try {
-        const riggingData = await prisma.riggingData.findUnique({
-          where: { id: craneData[0].riggingDataId },
-        });
-        const edgeDistance = await prisma.edgeDistance.findUnique({
-          where: { id: riggingData.edgeDistanceId },
-        });
-        const workBuilding = await prisma.workBuilding.findUnique({
-          where: { id: riggingData.workBuildingId },
-        });
-        const block = await prisma.block.findUnique({
-          where: { id: riggingData.blockId },
-        });
-        console.log(craneData[0], { riggingData, edgeDistance, workBuilding, block });
+        return await Promise.all(
+          craneData.map(async (craneInfo) => {
+            const riggingData = await prisma.riggingData.findUnique({ where: { id: craneInfo.riggingDataId } });
+            const edgeDistance = await prisma.edgeDistance.findUnique({ where: { id: riggingData.edgeDistanceId } });
+            const workBuilding = await prisma.workBuilding.findUnique({ where: { id: riggingData.workBuildingId } });
+            const block = await prisma.block.findUnique({ where: { id: riggingData.blockId } });
+            return {
+              craneName: craneInfo.craneName,
+              craneCode: craneInfo.craneCode,
+              craneModeName: craneInfo.craneModeName,
+              excelSheetName: craneInfo.excelSheetName,
+              craneData: {
+                mainBoom: riggingData.mainBoom,
+                mainAngle: riggingData.mainAngle,
+                totalExtLength: riggingData.totalExtLength,
+                adapter1: riggingData.adapter1,
+                extBoom1: riggingData.extBoom1,
+                extBoom2: riggingData.extBoom2,
+                extBoom3: riggingData.extBoom3,
+                extBoom4: riggingData.extBoom4,
+                adapter2: riggingData.adapter2,
+                flyFixLuffing: riggingData.flyFixLuffing,
+                flyFixLuffingAngle: riggingData.flyFixLuffingAngle,
+                distance1: riggingData.distance1,
+                distance2: riggingData.distance2,
+                craneDistance: riggingData.craneDistance,
+                edgeDistance: {
+                  mainToBlock: edgeDistance.mainToBlock,
+                  mainToBuilding: edgeDistance.mainToBuilding,
+                  flyFixLuffingToBlock: edgeDistance.flyFixLuffingToBlock,
+                  flyFixLuffingToBuilding: edgeDistance.flyFixLuffingToBuilding,
+                },
+                centerToBuildingDistance: riggingData.centerToBuildingDistance,
+                centerToBlockDistance: riggingData.centerToBlockDistance,
+                craneToBuildingDistance: riggingData.craneToBuildingDistance,
+                craneToBlockDistance: riggingData.craneToBlockDistance,
+                totalDistance: riggingData.totalDistance,
+                tableDistance: riggingData.tableDistance,
+                height1: riggingData.height1,
+                height2: riggingData.height2,
+                totalHeight: riggingData.totalHeight,
+                marginHeight: riggingData.marginHeight,
+                workingArea: riggingData.workingArea,
+                tableWeight: riggingData.tableWeight,
+                counterWeight: riggingData.counterWeight,
+                overRear: riggingData.overRear,
+                optional: riggingData.optional,
+                safetyFactor: riggingData.safetyFactor,
+                craneLocation: riggingData.craneLocation,
+                workWeight: riggingData.workWeight,
+                workBuilding: {
+                  horizontal: workBuilding.horizontal,
+                  vertical: workBuilding.vertical,
+                  height: workBuilding.height,
+                },
+                block: {
+                  vertical1: block.vertical1,
+                  horizontal1: block.horizontal1,
+                  height1: block.height1,
+                  vertical2: block.vertical2,
+                  height2: block.height2,
+                },
+              },
+            };
+            // return loadRiggingData;
+          })
+        );
+        // console.log(loadCraneData);
+        // return loadCraneData;
       } catch (e) {
         throw new Error(e);
       }
--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -6,17 +6,19 @@ export default {
   Mutation: {
     editUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      let { email, name, phoneNumber } = args;
+      let { email, name, phoneNumber, avatar } = args;
       const { user } = request;
       if (!email) email = user.email;
       if (!name) name = user.name;
       if (!phoneNumber) phoneNumber = user.phoneNumber;
+      if (!avatar) avatar = user.avatar;
       return await prisma.user.update({
         where: { id: user.id },
         data: {
           email,
           name,
           phoneNumber,
+          avatar,
         },
       });
     },
