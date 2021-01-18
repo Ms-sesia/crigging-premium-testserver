f2fa8aafd9cd3df7b2e77383b599e8777057222b
Ms-sesia <zoenea@naver.com>
2021-01-18 16:45:33 +0900

    Post
    * Post
     - try catch added.
     - commentCount, likeCount resolver modified.

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -5,9 +5,33 @@ const prisma = new PrismaClient();
 export default {
   Post: {
     postAuthor: async (parent) => await prisma.post.findUnique({ where: { id: parent.id } }).postAuthor(),
-    files: async (parent) => await prisma.post.findUnique({ where: { id: parent.id } }).files(),
-    comments: async (parent) => await prisma.post.findUnique({ where: { id: parent.id } }).comments(),
-    likes: async (parent) => await prisma.post.findUnique({ where: { id: parent.id } }).likes(),
+    files: async (parent) => {
+      try {
+        const result = await prisma.post.findUnique({ where: { id: parent.id } }).files();
+        return result;
+      } catch (e) {
+        console.log(e);
+        throw new Error("파일정보를 불러오는데 실패하였습니다.");
+      }
+    },
+    comments: async (parent) => {
+      try {
+        const result = await prisma.post.findUnique({ where: { id: parent.id } }).comments();
+        return result;
+      } catch (e) {
+        console.log(e);
+        throw new Error("댓글정보를 불러오는데 실패하였습니다.");
+      }
+    },
+    likes: async (parent) => {
+      try {
+        const result = await prisma.post.findUnique({ where: { id: parent.id } }).likes();
+        return result;
+      } catch (e) {
+        console.log(e);
+        throw new Error("좋아요 정보를 불러오는데 실패하였습니다.");
+      }
+    },
     isLiked: async (parent, __, { request }) => {
       const { user } = request;
       const likeResult = await prisma.like.findMany({
@@ -19,9 +43,8 @@ export default {
     },
     commentCount: async (parent) => {
       try {
-        const commentCount = await prisma.post.findMany({
-          where: { comments: { some: { postId: parent.id } } },
-          select: { id: true },
+        const commentCount = await prisma.comment.findMany({
+          where: { postId: parent.id },
         });
         return commentCount.length;
       } catch (e) {
@@ -30,9 +53,8 @@ export default {
     },
     likeCount: async (parent) => {
       try {
-        const likeCount = await prisma.post.findMany({
-          where: { likes: { some: { postId: parent.id } } },
-          select: { id: true },
+        const likeCount = await prisma.like.findMany({
+          where: { postId: parent.id },
         });
         return likeCount.length;
       } catch (e) {
